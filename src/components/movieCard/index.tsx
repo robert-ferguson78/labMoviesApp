fe73c1ac3f424af73c340d6d9ef67d8c82cb10c2
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-06-05 17:38:54 +0100

    Added routing and live data from API.

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -12,7 +13,7 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import img from '../../images/film-poster-placeholder.png';
-import { BaseMovieProps } from "../../types/interfaces"; 
+import { BaseMovieProps } from "../../types/interfaces";
 
 const styles = {
   card: { maxWidth: 345 },
@@ -56,9 +57,11 @@ const MovieCard: React.FC<BaseMovieProps> = (movie) => {
         <IconButton aria-label="add to favorites" >
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
