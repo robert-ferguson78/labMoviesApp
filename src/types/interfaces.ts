83727c29ce078d8b35a9c8d4d140c42a8e281cb0
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-06-07 18:08:26 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -20,6 +20,7 @@ export interface BaseMovieProps {
 
   export interface BaseMovieListProps { 
     movies: BaseMovie[];
+    selectFavourite: (movieId: number) => void;
   }
 
   export interface MovieDetailsProps extends BaseMovieProps {
