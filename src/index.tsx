1869afd920997b8626f468c588b1d3cfbfa76bd8
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-06-15 19:34:21 +0100

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import UpcomingPage from "./pages/upcomingPage";
 import SiteHeader from './components/siteHeader'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<UpcomingPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
