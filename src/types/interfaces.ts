adff128b36f33a624aba36a7aa6d684fa905737e
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-06-15 18:55:22 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -54,3 +54,9 @@ export interface BaseMovieProps {
   export interface MovieListPageTemplateProps extends BaseMovieListProps {
     title: string;
   }
+
+  export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
