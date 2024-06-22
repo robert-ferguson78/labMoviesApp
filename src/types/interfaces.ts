e093a6bc5a7c02ab03a1d4c680a7e234a9b42b86
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-06-22 21:02:10 +0100

    Removed interface duplicate of Review

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -55,12 +55,6 @@ export interface BaseMovieProps {
     title: string;
   }
 
-  export interface Review{
-    id: string;
-    content: string
-    author: string
-  }
-
   export interface GenreData {
     genres: {
       id: string;
